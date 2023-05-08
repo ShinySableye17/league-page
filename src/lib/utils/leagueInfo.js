7d52481c5ac508f8d68229df6e9af4c21e17e31b
ShinySableye17 <128442649+ShinySableye17@users.noreply.github.com>
2023-05-08 13:57:44 -0400

    Update leagueInfo.js
    changed Eshaan and Scrappy to "Win Now" mode

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -216,7 +216,7 @@ export const managers = [
       "photo": "/managers/Eshaan.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "lac", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: null, // Can be anything (usually your rival's name)
         link: null, // manager array number within this array, or null to link back to all managers page
@@ -239,7 +239,7 @@ export const managers = [
       "photo": "/managers/Scrappy.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": null, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "dal", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": null, // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: null, // Can be anything (usually your rival's name)
         link: null, // manager array number within this array, or null to link back to all managers page
