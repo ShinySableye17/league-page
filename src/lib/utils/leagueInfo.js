a50a6aa7ba83b0221d6c0a020fc40a18420687bc
ShinySableye17 <128442649+ShinySableye17@users.noreply.github.com>
2023-05-27 13:10:48 -0400

    Fixed a typo on Rabz's page
    apparently I didn't capitalize the W in "Win Now" before, oops.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -201,7 +201,7 @@ export const managers = [
       "photo": "/managers/Rabz.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2017, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "chi", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
-      "mode": "win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
+      "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
       "rival": {
         name: "Bronson", // Can be anything (usually your rival's name)
         link: 10, // manager array number within this array, or null to link back to all managers page
