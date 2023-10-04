093cffb6100d315e15be636670bedb47dfaea330
ShinySableye17 <128442649+ShinySableye17@users.noreply.github.com>
2023-10-04 03:45:05 -0400

    Manager page tweak
    Changed my desire to trade from 9 down to 6, because I'm starting to realize that I don't want to trade as much as I thought.

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -118,7 +118,7 @@ export const managers = [
       "valuePosition": "TE", // (optional) Favorite position (QB, WR, RB, TE, etc.)
       "rookieOrVets": "Rookies", // (optional) 'Rookies' or 'Vets' (anything else and you will need to add a new png to /static/ similar to the 'Rookies.png' and 'Vets.png' currently in there)
       "philosophy": null,
-      "tradingScale": 9, // 1 - 10
+      "tradingScale": 6, // 1 - 10
       "preferredContact": "Discord", // 'Text', 'WhatsApp', 'Sleeper', 'Email', 'Phone', 'Discord', and 'Carrier Pigeon' are currently supplied in the template
     },
     {
